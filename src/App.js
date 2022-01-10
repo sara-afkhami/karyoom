02bdd7c72b3f884cccc4af484c2a325cb9e71670
sara-afkhami <s.afkhami1999@gmail.com>
2022-01-10 22:49:21 +0330

    components imported

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React, { Component } from "react";
-import "./App.css";
 import { Switch, Route, withRouter } from "react-router-dom";
-
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-
 import "./pageTransitions/slideTransition.scss";
 import StepOnePage from "./pages/stepOnePage";
 import StepTwoPage from "./pages/stepTwoPage";
 import StepThreePage from "./pages/stepThreePage";
 import StepFourPage from "./pages/stepFourPage";
+import StepFivePage from "./pages/stepFivePage";
+import StepSixPage from "./pages/stepSixPage";
+import StepSevenPage from "./pages/stepSevenPage";
+import StepEightPage from "./pages/stepEightPage";
+import StepNinePage from "./pages/stepNinePage";
+import StepTenPage from "./pages/stepTenPage";
+import StepElevenPage from "./pages/stepElevenPage";
+import "./App.css";
 
 class App extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    prevDepth: this.getPathDepth(this.props.location)
-  };
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      prevDepth: this.getPathDepth(this.props.location),
+    };
+  }
 
-componentWillReceiveProps() {
-  this.setState({ prevDepth: this.getPathDepth(this.props.location) });
-}
+  componentWillReceiveProps() {
+    this.setState({ prevDepth: this.getPathDepth(this.props.location) });
+  }
 
-getPathDepth(location) {
-  let pathArr = location.pathname.split("/");
-  pathArr = pathArr.filter(n => n !== "");
-  return pathArr.length;
-}
+  getPathDepth(location) {
+    let pathArr = location.pathname.split("/");
+    pathArr = pathArr.filter((n) => n !== "");
+    return pathArr.length;
+  }
 
   render() {
     const { location } = this.props;
@@ -43,20 +48,27 @@ getPathDepth(location) {
           mountOnEnter={false}
           unmountOnExit={true}
         >
-<div
-  className={
-    this.getPathDepth(location) - this.state.prevDepth >= 0
-      ? "left"
-      : "right"
-  }
->
-  <Switch location={location}>
-    <Route path="/" exact component={StepOnePage} />
-    <Route path="/step2" exact component={StepTwoPage} />
-    <Route path="/step3" exact component={StepThreePage}/>
-    <Route path="/step4" exact component={StepFourPage}/>
-  </Switch>
-</div>
+          <div
+            className={
+              this.getPathDepth(location) - this.state.prevDepth >= 0
+                ? "left"
+                : "right"
+            }
+          >
+            <Switch location={location}>
+              <Route path="/" exact component={StepOnePage} />
+              <Route path="/step2" exact component={StepTwoPage} />
+              <Route path="/step3" exact component={StepThreePage} />
+              <Route path="/step4" exact component={StepFourPage} />
+              <Route path="/step5" exact component={StepFivePage} />
+              <Route path="/step6" exact component={StepSixPage} />
+              <Route path="/step7" exact component={StepSevenPage} />
+              <Route path="/step8" exact component={StepEightPage} />
+              <Route path="/step9" exact component={StepNinePage} />
+              <Route path="/step10" exact component={StepTenPage} />
+              <Route path="/step11" exact component={StepElevenPage} />
+            </Switch>
+          </div>
         </CSSTransition>
       </TransitionGroup>
     );
--- src/pages/stepElevenPage.jsx
+++ src/pages/stepElevenPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 const success = () => {
   toast("اطلاعات شما با موفقیت دریافت شد");
